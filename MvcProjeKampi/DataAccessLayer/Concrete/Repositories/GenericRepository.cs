23702d637b8b6fc4c12a2c80a30facfe3a10fb0e
mustafaalpyanikoglu <79158705+mustafaalpyanikoglu@users.noreply.github.com>
2022-08-30 13:10:38 +0300

    feat: added generic repository

---
+++ MvcProjeKampi/DataAccessLayer/Concrete/Repositories/GenericRepository.cs
@@ -0,0 +1,49 @@
+﻿using DataAccessLayer.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Concrete.Repositories
+{
+    public class GenericRepository<T> : IRepository<T> where T : class
+    {
+        Context context = new Context();
+        DbSet<T> _object;
+
+        public GenericRepository()
+        {
+            _object = context.Set<T>();
+        }
+
+        public void Delete(T item)
+        {
+            _object.Remove(item);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll()
+        {
+            return _object.ToList();
+        }
+
+        public void Insert(T item)
+        {
+            _object.Add(item);
+            context.SaveChanges();
+        }
+
+        public List<T> GetAll(Expression<Func<T, bool>> filter)
+        {
+            return _object.Where(filter).ToList();
+        }
+
+        public void Update(T item)
+        {
+            context.SaveChanges();
+        }
+    }
+}
